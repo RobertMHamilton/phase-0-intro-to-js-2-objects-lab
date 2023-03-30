2dd847c70819a19584897f90ad8e99db7582580e
Robert Hamilton <Robertmp1@icloud.com>
2023-03-30 14:36:17 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+var employee = {
+  name : "Sam",
+  streetAddress : "11 Broadway",
+
+};
+function updateEmployeeWithKeyAndValue (object, key, value){
+  const newObj = { ...object };
+  newObj[key] = value;
+  return newObj;
+};
+ 
+function deleteFromEmployeeByKey (object, key, value){
+  const newObj2 = { ...object };
+  newObj2[key] = value;
+  return newObj2;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+};
+
+function destructivelyDeleteFromEmployeeByKey (object, key, value) {
+  delete object[key];
+  return object;
+};
+
